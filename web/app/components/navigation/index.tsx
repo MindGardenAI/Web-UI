0e8d891cde4235f735087c15818fca411c10af2b
EliKowal <122854851+EliKowal@users.noreply.github.com>
2024-03-16 19:09:21 -0400

    Navbar Color

--- web/app/components/navigation/index.tsx
+++ web/app/components/navigation/index.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+      <div className="w-full h-20 navbar-color sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <Logo />
