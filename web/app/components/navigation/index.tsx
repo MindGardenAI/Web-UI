7e5259e5c55f6f122360bef931e662e7a52dceac
Corey <clivinghouse@mertandhouse.com>
2024-03-16 19:22:11 -0400

    changed nav bar

--- web/app/components/navigation/index.tsx
+++ web/app/components/navigation/index.tsx
@@ -12,36 +12,34 @@ const Navbar = () => {
   const {isAuthenticated} = useAuth0()
 
   return (
-    <>
-      <div className="w-full h-20 navbar-color sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <Logo />
-            <ul className="hidden md:flex gap-x-6 text-white">
-             <ul>
-             {!isAuthenticated ? <a href="/login">Login</a> : <LogoutButton/>}
-             </ul>
-              {/* <li>
-                <Link href="/about">
-                  <p>About Us</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services">
-                  <p>Services</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contacts">
-                  <p>Contacts</p>
-                </Link>
-              </li> */}
-            </ul>
-             
-          </div>
-        </div>
-      </div>
-    </>
+    <header className="header sticky top-0 navbar-color shadow-md flex items-center justify-   px-8 py-02">
+    <h1 className="w-3/12">
+        <a href="/">
+          <Logo></Logo>
+        </a>
+    </h1>
+
+    <nav className="nav navbar-color font-semibold text-lg">
+        <ul className="flex items-center">
+            <li className="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer active">
+              <a href="">Accueil</a>
+            </li>
+            <li className="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+              <a href="">Produits</a>
+            </li>
+            <li className="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+              <a href="">Collections</a>
+            </li>
+            <li className="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+              <a href="">Contact</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div className="w-3/12 flex justify-end">
+    {!isAuthenticated ? <a href="/login">Login</a> : <LogoutButton/>}
+    </div>
+</header>
   );
 };
 
