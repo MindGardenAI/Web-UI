3b29f91f1d93f9ff27d379748d1a4b361955bcd7
EliKowal <122854851+EliKowal@users.noreply.github.com>
2024-03-16 19:05:06 -0400

    .

--- web/app/components/navigation/index.tsx
+++ web/app/components/navigation/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import Logo from "./logo";
+import { useAuth0 } from "@auth0/auth0-react";
 // import Button from "./button";
 
 const Navbar = () => {
