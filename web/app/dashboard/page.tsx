3038953373f2ffc9d7da5c623e3f5dcbbde689a8
Corey <clivinghouse@mertandhouse.com>
2024-03-16 16:01:07 -0400

    added all available pages

---
+++ web/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+export default function dashboard() {
+    return (
+    <>
+   <div className="container mx-auto px-20">
+    <p>Dashboard</p>
+   </div>
+    </>
+    );
+  }
---
+++ web/app/goals-tree/page.tsx
@@ -0,0 +1,10 @@
+
+export default function goalsTree() {
+    return (
+    <>
+   <div className="container mx-auto px-20">
+    <p>Goals/Tree</p>
+   </div>
+    </>
+    );
+  }
