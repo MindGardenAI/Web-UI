07a042a434587a6f6e9be59d21f9781895149c1d
Corey <clivinghouse@mertandhouse.com>
2024-03-16 21:26:56 -0400

    added card content

--- web/app/goals-tree/page.tsx
+++ web/app/goals-tree/page.tsx
@@ -1,10 +1,31 @@
-
+import Image from "next/image";
+import "./css.css";
+import TreeLeaf from "../components/TreeLeaf/TreeLeaf";
 export default function goalsTree() {
-    return (
-    <>
-   <div className="container mx-auto px-20">
-    <p>Goals/Tree</p>
-   </div>
-    </>
-    );
-  }
+  return (
+    <div className="container mx-auto px-4">
+  <div className="text-center">
+    <h1 className="big">Goals</h1>
+  </div>
+  <div className="flex  absolute inset-x-0 bottom-0 justify-center">
+    <div className="flex flex-col">
+    <div className="mb-6">
+    <TreeLeaf title="Test" content="This is ndodwduwhduiediehduiohoduihduwehudiewhudiewh"/>
+    </div>
+    <div className="flex justify-center">
+    <Image
+      src="/images/potplant.png"
+      width={200}
+      height={200}
+      alt="plant"
+      className="flex justify-self-center"
+    />
+    </div>
+    
+    </div>
+    
+  </div>
+</div>
+
+  );
+}
