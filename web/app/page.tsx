7e4938047b0e754d68286c60231ce0143ebb9cbf
Dradeon <69202556+Dradeon@users.noreply.github.com>
2024-03-16 22:12:40 -0400

    Added ui components to home page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,13 +4,22 @@ import heroImage from '../public/images/heroimage.png'
 
 export default function Page() {
   return (
-  <>
-    <div className="container mx-auto text-center my-10">
-      <h1 className="font-thin text-8xl">Grow your Happiness</h1>
-      <h2 className="font-thin">Set goals, journal, while practicing thoughtfulness and gratitude</h2>
+  <div className='flex flex-col'>
+    <div className="container mx-auto text-center mt-20 mb-10">
+      <h1 className="font-thin text-[2.5rem] sm:text-[3.5rem] lg:text-[5.5rem]">Grow your Happiness</h1>
+      <h1 className="font-thin text-xl sm:text-2xl lg:text-3xl">Set goals, journal, while practicing thoughtfulness and gratitude</h1>
     </div>
-    <button className="bg-[var(--primary-color)] w-[196px] h-[128px]">Get Started</button>
-    <Image className='rounded-full' src={heroImage} width={600} height={600} alt="Woman surrounded by plants" />
-  </>
+    <button className="font-bold bg-[var(--primary-color)] w-[156px] h-[76px] rounded-2xl mx-auto">Get Started</button>
+    <div className='mx-auto w-[67%]'>
+    <Image className=' mx-auto rounded-full drop-shadow-lg my-10' src={heroImage} width={480} height={480} alt="Woman surrounded by plants" />
+    </div>
+    <div className='mx-auto text-center'>
+      <h1 className='text-sm'>Professional Health Resources:</h1>
+      <span className='text-sm'>Home | NAMI: National Alliance on Mental Illness</span>
+      <hr className='border-black border-1 mb-4'/>
+      <p className='text-sm'>National Alliance on Mental Illness (NAMI) Helpline:</p>
+      <span className='text-sm'>1-800-950-NAMI (1-800-950-6264)</span>
+    </div>
+  </div>
   );
 }
