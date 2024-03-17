9d26acaa0760d9ecb2061d048bce94205d766a88
EliKowal <122854851+EliKowal@users.noreply.github.com>
2024-03-16 23:27:58 -0400

    comit

--- web/app/journal/studies.tsx
+++ web/app/journal/studies.tsx
@@ -2,7 +2,7 @@ const Studies = () => {
   return (
     <div>
         <p>Studies show that practicing gratitude has a multitude of mental health benefits</p>
-        <a href='' target="_blank">The Science of Gratitude - Mindful</a>
+        <a href='https://www.mindful.org/the-science-of-gratitude/' target="_blank">The Science of Gratitude - Mindful</a>
     </div>
   )
 }
