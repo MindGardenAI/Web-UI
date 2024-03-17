0b71de6928cbbb03e20dfbb30366db9251f2bb55
Corey <clivinghouse@mertandhouse.com>
2024-03-17 13:29:32 -0400

    done

--- web/app/goals-tree/useGetGoals.ts
+++ web/app/goals-tree/useGetGoals.ts
@@ -1,13 +1,17 @@
 import { useQuery } from "react-query"
 
+const API_URL = process.env['NEXT_PUBLIC_API_URL']
+
 const getGoals = () => {
-    return fetch('',{
-        method: 'GET',
+    return fetch(`${API_URL}/get_user_goals`,{
+        method: 'POST',
         headers: {
             "Content-Type": 'application/json'
         },
-        body: JSON.stringify({})
-    })
+        body: JSON.stringify({
+          "uid":"google-oauth2|104493244220651796441"
+        })
+    }).then((res)=>{return res.json()})
 }
 
 const useGetGoals = () => {
