7b324bb9498649a084d3d673582d15aac258ed31
EliKowal <122854851+EliKowal@users.noreply.github.com>
2024-03-17 11:15:20 -0400

    Added Shadows

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -5,11 +5,11 @@ import LoginButton from "../components/loginButton/loginbutton";
 const Login = () => {
 
   return (<div className="flex">
-    <div className="container max-w-[480px] bg-[var(--primary-color)] rounded-2xl mx-auto mt-40 p-4 pb-8">
+    <div className="container drop-shadow-lg max-w-[480px] bg-[var(--primary-color)] rounded-2xl mx-auto mt-40 p-4 pb-8">
       <div className="w-full justify-center">
-        <h1 className="text-5xl text-center my-10">Login</h1>
+        <h1 className="text-5xl text-center my-10">LOGIN</h1>
       </div>
-      <div className="container flex">
+      <div className="container drop-shadow-lg flex">
         <div className="mx-auto">
           <LoginButton/>
         </div>
