5d14da18615dd9cf574237df168b158e36519af8
EliKowal <122854851+EliKowal@users.noreply.github.com>
2024-03-17 12:53:34 -0400

    Added Title to Front Page

--- web/app/components/navigation/index.tsx
+++ web/app/components/navigation/index.tsx
@@ -6,57 +6,47 @@ import Logo from "./logo";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "../logoutButton/logoutbutton";
 import defaultAvatar from '../../../public/images/Default Avatar.png'
-
-type Linktype = {
-  id: number,
-  link: string,
-  title: string
-}
-
-var links = [
-  {
-    id: 1,
-    link: "/",
-    title:"Home"
-  },
-  {
-    id:2,
-    link:"/goals-tree",
-    title:"Goals"
-  },
-  {
-    id:3,
-    link:"/journal",
-    title:"Journal"
-  }]
-
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const {user, isAuthenticated} = useAuth0()
     const profile = user?.picture || defaultAvatar;
-  
 
+  const links = [
+    {
+      id: 1,
+      link: "/",
+      title:"Home"
+    },
+    {
+      id:2,
+      link:"/goals-tree",
+      title:"Goals"
+    },
+    {
+      id:3,
+      link:"/journal",
+      title:"Journal"
+    }
+    
+  ];
 
   return (
     <div className="flex z-10 justify-between items-center w-full h-20 px-4 text-white drop-shadow-xl bg-[var(--primary-color)] fixed nav">
       <div>
           <Logo/>
       </div>
+      <h1 className="flex z-10 justify-between items-center w-full h-20 px-4 text-2xl text-black">Mind Garden AI</h1> 
 
       <ul className="hidden md:flex items-center">
       
-        {links.map(({ id, link, title }) => {
-            if(id !== 1 && isAuthenticated){
-              return (
-                <li
-                  key={id}
-                  className="nav-links px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-white duration-200 link-underline"
-                >
-                  <a href={link}>{title}</a>
-                </li>
-              )
-            }
-            return <li key={id}></li>})}
+        {links.map(({ id, link, title }) => (
+          <li
+            key={id}
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-white duration-200 link-underline"
+          >
+            <a href={link}>{title}</a>
+          </li>
+        ))}
         {!isAuthenticated && <li className="nav-links px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-white duration-200 link-underline"><a href="/login">Login</a></li>}
         {isAuthenticated && <li className="nav-links px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-white duration-200 link-underline"><LogoutButton/></li>}
         {isAuthenticated && <li><a href='/dashboard'><Image className="w-[40px] h-[40px] ml-2 rounded-full" src={profile} width={40} height={40} alt='profile picture'></Image></a></li>}
@@ -84,6 +74,6 @@ const Navbar = () => {
       }
     </div>
   );
-}
+};
 
 export default Navbar;
