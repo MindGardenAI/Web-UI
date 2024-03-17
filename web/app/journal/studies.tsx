5191323a4c9d670809cb2d363478867dd1571310
Dradeon <69202556+Dradeon@users.noreply.github.com>
2024-03-16 23:15:55 -0400

    Fixed hydration eror

--- web/app/journal/studies.tsx
+++ web/app/journal/studies.tsx
@@ -1,8 +1,9 @@
+
 const Studies = () => {
   return (
     <div>
         <p>Studies show that practicing gratitude has a multitude of mental health benefits</p>
-        <a href='' target="_blank">The Science of Gratitude - Mindful</a>
+        <a href='/' target="_blank">The Science of Gratitude - Mindful</a>
     </div>
   )
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from 'next/image'
+import Link from 'next/link'
 import heroImage from '../public/images/heroimage.png'
 
 export default function Page() {
@@ -9,7 +10,9 @@ export default function Page() {
       <h1 className="font-thin text-[2.5rem] sm:text-[3.5rem] lg:text-[5.5rem]">Grow your Happiness</h1>
       <h1 className="font-thin text-xl sm:text-2xl lg:text-3xl">Set goals, journal, while practicing thoughtfulness and gratitude</h1>
     </div>
-    <button className="font-bold bg-[var(--primary-color)] w-[156px] h-[76px] rounded-2xl mx-auto">Get Started</button>
+    <div className='mx-auto'>
+      <Link href='/signup'><button className="font-bold bg-[var(--primary-color)] w-[156px] h-[76px] rounded-2xl">Get Started</button></Link>
+    </div>
     <div className='mx-auto w-[67%] z-[-1]'>
     <Image className=' mx-auto rounded-full drop-shadow-lg my-10' src={heroImage} width={480} height={480} alt="Woman surrounded by plants" />
     </div>
