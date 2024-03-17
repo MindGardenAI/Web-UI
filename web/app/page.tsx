ff1c2d6813514c2437cf79db6a860ef9bd95acdc
Dradeon <69202556+Dradeon@users.noreply.github.com>
2024-03-16 22:39:23 -0400

    Fixed nav

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,12 +5,12 @@ import heroImage from '../public/images/heroimage.png'
 export default function Page() {
   return (
   <div className='flex flex-col'>
-    <div className="container mx-auto text-center mt-20 mb-10">
+    <div className="container mx-auto text-center mt-40 mb-10">
       <h1 className="font-thin text-[2.5rem] sm:text-[3.5rem] lg:text-[5.5rem]">Grow your Happiness</h1>
       <h1 className="font-thin text-xl sm:text-2xl lg:text-3xl">Set goals, journal, while practicing thoughtfulness and gratitude</h1>
     </div>
     <button className="font-bold bg-[var(--primary-color)] w-[156px] h-[76px] rounded-2xl mx-auto">Get Started</button>
-    <div className='mx-auto w-[67%]'>
+    <div className='mx-auto w-[67%] z-[-1]'>
     <Image className=' mx-auto rounded-full drop-shadow-lg my-10' src={heroImage} width={480} height={480} alt="Woman surrounded by plants" />
     </div>
     <div className='mx-auto text-center'>
